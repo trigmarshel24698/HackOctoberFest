f78a09ad5773f8737cecb8856df77b642dd67806
Lise Ho <koprty@users.noreply.github.com>
2018-01-29 12:36:12 -0500

    adding dummy python file for testing

---
+++ mysteryscore.py
@@ -0,0 +1,17 @@
+import datetime
+
+#This is the my mystery function that will give a score to each string
+def mystery_score(mystr):
+
+    # z is the highest character we want - 
+    totalScore = ord("z")
+    charsum = 1
+    for ch in mystr:
+        charsum += ord(ch)
+
+    return charsum/totalScore
+
+if __name__ == "__main__":
+    string= raw_input("What is the string you want to score? ")
+    print "Your score for '" + string + "' is "+ str(mystery_score(string))
+    
